af90a20b06fb5cc9115b419bdf026167e831de11
Eduardo de la Cruz Ibarra <eduardoibarra904@gmail.com>
2018-07-20 18:07:37 -0500

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import {Route, RouterModule} from '@angular/router';
+const routes: Route[] = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'form', component: FormComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
